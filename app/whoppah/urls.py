bd16acaa540e72f427dd979c4457d4024687b2a9
Omer <oalkin@lequest.com>
2023-06-19 01:29:03 +0200

    First commit & dockerized basic django app

---
+++ app/whoppah/urls.py
@@ -0,0 +1,34 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Whoppah Second hand market API",
+        default_version="v1",
+        description="All the APIs and the models can be found.",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    url(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    url(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    url(
+        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
+]
