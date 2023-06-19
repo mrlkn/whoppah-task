cd20b243d4e669f1e9e3ea6e7c2f69eb7c339ed6
Omer <oalkin@lequest.com>
2023-06-19 15:34:58 +0200

    Added logic to CategoryViewSet

--- app/product/serializers.py
+++ app/product/serializers.py
@@ -1,11 +1,38 @@
+from typing import Union
+
 from rest_framework import serializers
+
 from .models import Category, Product
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    created_by = serializers.SerializerMethodField()
+    last_updated_by = serializers.SerializerMethodField()
+
     class Meta:
         model = Category
-        fields = ['id', 'title', 'slug']
+        fields = "__all__"
+        read_only_fields = ('slug',)
+
+    def create(self, validated_data: dict) -> Category:
+        user = self.context['request'].user
+        category = Category.objects.create(created_by=user, **validated_data)
+        return category
+
+    def update(self, instance: Category, validated_data: dict) -> Category:
+        user = self.context['request'].user
+        instance.last_updated_by = user
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
+
+    def get_created_by(self, obj: Category) -> Union[str]:
+        return obj.created_by.email if obj.created_by else None
+
+    def get_last_updated_by(self, obj: Category) -> Union[str]:
+        return obj.last_updated_by.email if obj.last_updated_by else None
 
 
 class ProductSerializer(serializers.ModelSerializer):
